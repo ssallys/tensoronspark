15f0792fe45d16ff4be3434f302437519197da58
yeounhee <ssallys@naver.com>
2016-10-05 16:40:48 +0900

    tensoronspark with python3.5

--- tensorspark/example/run.py
+++ tensorspark/example/run.py
@@ -2,8 +2,10 @@ import sys
 import tensorspark.example.spark_mnist as mnist
 from pyspark import SparkContext
 
-sc = SparkContext()
-num_epoch = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+if __name__ == "__main__":
+	sc = SparkContext()
+	num_epoch = int(sys.argv[1]) if len(sys.argv) > 1 else 2
 
-mnist.train(sc=sc, user='etri', name='mnist_try', server_host='localhost', server_port=10080,
+	mnist.train(sc=sc, user='etri', name='mnist_try', server_host='localhost', server_port=10080,
                   sync_interval=100, batch_size=100, num_partition=1, num_epoch=num_epoch)
+	spark.stop()
