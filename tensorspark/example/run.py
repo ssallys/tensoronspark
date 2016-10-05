002fb7120021d039890831e09b61e4135e9bcb92
yeounhee <ssallys@naver.com>
2016-10-05 21:34:41 +0900

    adds run.py

--- tensorspark/example/run.py
+++ tensorspark/example/run.py
@@ -3,9 +3,9 @@ import tensorspark.example.spark_mnist as mnist
 from pyspark import SparkContext
 
 if __name__ == "__main__":
-	sc = SparkContext()
 	num_epoch = int(sys.argv[1]) if len(sys.argv) > 1 else 2
 
-	mnist.train(sc=sc, user='etri', name='mnist_try', server_host='localhost', server_port=10080,
+	sc = SparkContext()
+	mnist.train(sc=sc, user='etri', name='mnist_try', server_host='129.254.164.75', server_port=10080,
                   sync_interval=100, batch_size=100, num_partition=1, num_epoch=num_epoch)
-	spark.stop()
+	print('end of run')
